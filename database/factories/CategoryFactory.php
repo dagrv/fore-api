f3039cbbabca884ed49768ae0666d9f16700d6aa
rust <rust.91@icloud.com>
2021-03-19 15:13:18 +0100

    Category model setup + Tests

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+$factory->define(Category::class, function(Faker $faker) {
+    return [
+        'name' => $name = $faker->unique()->name,
+        'slug' => Str::slug($name),
+    ];
+});
