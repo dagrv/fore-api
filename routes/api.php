38fd2846835b7e161d9c8ed4c6661a6149cafc98
rust <rust.91@icloud.com>
2021-03-19 22:39:09 +0100

    Product Index Endpoint + Tests

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,8 @@
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
+// -- Categories -- 
 Route::resource('categories', 'Categories\CategoryController');
+
+// -- Products -- 
+Route::resource('products', 'Products\ProductController');
