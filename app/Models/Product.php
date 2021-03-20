7ebd86654c405798227c3e3d7d59442cfb4deb30
rust <rust.91@icloud.com>
2021-03-20 20:07:59 +0100

    Setup product variations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Scoping\Scoper;
+use App\Models\ProductVariation;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,5 +13,17 @@ class Product extends Model {
 
     public function getRouteKeyName() {
         return 'slug';
+    }
+
+    public function scopeWithScopes(Builder $builder, $scopes = []) {
+        return (new Scoper(request()))->apply($builder, $scopes);
+    }
+
+    public function categories() {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function variations() {
+        return $this->hasMany(ProductVariation::class)->orderBy('order', 'asc');
     }
 }
---
+++ database/factories/ProductVariationFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\ProductVariation;
+use Faker\Generator as Faker;
+
+$factory->define(ProductVariation::class, function(Faker $faker) {
+    return [
+        'product_id' => factory(Product::class)->create()->id,
+        'name' => $faker->unique()->name,
+    ];
+});
