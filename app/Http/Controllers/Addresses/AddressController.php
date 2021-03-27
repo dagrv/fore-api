976001b14a3a626bdc9ef9f59e75a233f407d4af
rust <rust.91@icloud.com>
2021-03-27 22:28:27 +0100

    Storing address

--- app/Http/Controllers/Addresses/AddressController.php
+++ app/Http/Controllers/Addresses/AddressController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Addresses;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Addresses\AddressStoreRequest;
 use App\Http\Resources\AddressResource;
+use App\Models\Address;
 use Illuminate\Http\Request;
 
 class AddressController extends Controller {
@@ -14,6 +16,18 @@ class AddressController extends Controller {
     public function index(Request $request) {
         return AddressResource::collection(
             $request->user()->addresses
+        );
+    }
+
+    public function store(AddressStoreRequest $request) {
+        $address = Address::make($request->only(
+            ['name', 'address_1', 'city', 'postal_code', 'country_id']
+        ));
+
+        $request->user()->addresses()->save($address);
+
+        return new AddressResource(
+            $address
         );
     }
 }
